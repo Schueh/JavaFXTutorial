f70d67a1c64d0dfa00c475d3d0f273579018b918
Manuel Sidler <manuel.sidler@gmx.ch>
2014-01-01 22:42:50 +0100

    Save and load data from a xml file

--- src/javafxtutorial/MainApp.java
+++ src/javafxtutorial/MainApp.java
@@ -2,19 +2,24 @@ package javafxtutorial;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.prefs.Preferences;
+
+import com.thoughtworks.xstream.XStream;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Dialogs;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafxtutorial.model.Person;
+import javafxtutorial.util.FileUtil;
 
 public class MainApp extends Application {
 
@@ -51,6 +56,10 @@ public class MainApp extends Application {
 			rootLayout = (BorderPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
+			
+			RootLayoutController controller = loader.getController();
+			controller.setMainApp(this);
+			
 			primaryStage.show();
 		} catch (IOException e) {
 			// Exception gets thrown if the fxml file could not be loaded
@@ -58,6 +67,11 @@ public class MainApp extends Application {
 		}
 		
 		showPersonOverview();
+		
+		File file = getPersonalFilePath();
+		if (file != null) {
+			loadPersonDataFromFile(file);
+		}
 	}
 	
 	public Stage getPrimaryStage(){
@@ -132,4 +146,41 @@ public class MainApp extends Application {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
+	public void loadPersonDataFromFile(File file) {
+		XStream xstream = new XStream();
+		xstream.alias("person", Person.class);
+		
+		try {
+			String xml = FileUtil.readFile(file);
+			
+			ArrayList<Person> personList = (ArrayList<Person>) xstream.fromXML(xml);
+			
+			personData.clear();
+			personData.addAll(personList);
+			
+			setPersonFilePath(file);
+		} catch (Exception e) {
+			Dialogs.showErrorDialog(primaryStage, 
+					"Could not load data from file:\n" + file.getPath(),
+					"Could not load data", "Error", e);
+		}
+	}
+	
+	public void savePersonDataToFile(File file) {
+		XStream xstream = new XStream();
+		xstream.alias("person", Person.class);
+		
+		ArrayList<Person> personList = new ArrayList<Person>(personData);
+		
+		String xml = xstream.toXML(personList);
+		try {
+			FileUtil.saveFile(xml, file);
+			
+			setPersonFilePath(file);
+		} catch (Exception e) {
+			Dialogs.showErrorDialog(primaryStage, "Could not save data to file:\n" + file.getPath(),
+					"Could not save data", "Error", e);
+		}
+	}
 }
