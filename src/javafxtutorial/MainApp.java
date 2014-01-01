e126509d9886c82101697df9b3e671d3b948da36
Manuel Sidler <manuel.sidler@gmx.ch>
2014-01-01 16:40:35 +0100

    Add some CSS styling

--- src/javafxtutorial/MainApp.java
+++ src/javafxtutorial/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -40,6 +41,7 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
+		this.primaryStage.getIcons().add(new Image("file:resources/iamges/Address_Book.png"));
 		
 		try {
 			// Load the root layout from the fxml file
